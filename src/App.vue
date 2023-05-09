081c4d6bcba6290d41f60c9c99fa8451e9fabd4b
samuelebumbello <samuelebumbello17@gmail.com>
2023-05-09 15:51:31 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import Header from './components/Header.vue';
+import Jumbotron from './components/Jumbotron.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
+
+export default{
+  name: 'App',
+  components:{
+    Header,
+    Jumbotron,
+    Main,
+    Footer,
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+</style>

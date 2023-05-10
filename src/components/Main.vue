f106c9da6ded3a0a40fb680beabcd42e60c6f459
samuelebumbello <samuelebumbello17@gmail.com>
2023-05-10 17:52:49 +0200

    [add] main-jumbo

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -21,6 +21,23 @@ export default{
                 <p class="text-secondary text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laboriosam error perspiciatis cumque! Illo vel, delectus laudantium repudiandae natus consequatur!</p>
             </div>
         </div>
+        <div class="news-container d-flex justify-content-center align-items-center">
+            <div class="news-text">
+                <a class="fw-bold" href="">VIEW ALL LATEST NEWS</a>
+            </div>
+        </div>
+        <div class="main-jumbo d-flex justify-content-center align-items-center">
+            <div class="song">
+                <p class="text-white text-center">“I just listened to it and said to myself, “Man, I really love this album.” Still, today, it just sounds so fresh. It sounds full of ideas. These guys knew what they were doing. They’re good. And they’re inventive. I haven’t heard anything this year that’s as inventive. I don’t really expect to.”</p>
+                <div class="button-yt d-flex justify-content-center mt-5">
+                    <button class="btn-yt">
+                        <a href="https://www.youtube.com/watch?v=aL0dOoLpf7M">
+                            <img src="https://avada.website/music/wp-content/uploads/sites/68/2016/07/band_interview_play_icon.png"  alt="">
+                        </a>
+                    </button>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
@@ -33,6 +50,7 @@ export default{
     padding: 70px;
 }
 .cont-main{
+    height: 1000px;
     width: 100%;
     background-color: $quaternary-color;
 
@@ -51,6 +69,40 @@ export default{
 }
 .text-secondary{
     width: 850px;
+}
+
+.news-container{
+    width: 100%;
+    height: 160px;
+    background-color: $primary-color;
+    transition: all 0.3s;
+    a{
+        color: white;
+        text-decoration: none;
+        font-size: 20px;
+    }
+    &:hover{
+            background-color: white;
+            color: black;
+        }
+}
+
+.main-jumbo{
+    width: 100%;
+    height: 100vh;
+    background-image: url(../assets/img/home-testimonial-parallax.jpg);
+    .song{
+        width: 50%;
+        p{
+            font-size: 25px;
+            line-height: 44px;
+            letter-spacing: 2px;
+        }
+        .btn-yt{
+            background-color: transparent;
+            border: 0;
+        }
+    }
 }
 
 
